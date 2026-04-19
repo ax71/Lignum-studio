85bd97b6f7175a9647cd42c53426c6abe59badb1
I kadek Buktiasa <146343510+ax71@users.noreply.github.com>
2026-04-19 20:22:40 +0800

    refactor: remove legacy non-locale pages and components

--- src/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-import AboutPage from "./_components/AboutPage";
-
-export default function About() {
-  return <AboutPage />;
-}
--- src/app/home/index.tsx
+++
@@ -1,5 +0,0 @@
-import HeroSection from "./_components/HeroSections";
-
-export default function HomePage() {
-  return <HeroSection />;
-}
